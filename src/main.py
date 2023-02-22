a99b96af75b5e97f542226fcf9358a1c144e84f9
Chespins <sync.chespins@gmail.com>
2023-02-22 23:15:32 +0900

    test

--- src/main.py
+++ src/main.py
@@ -1,30 +1,37 @@
 # -*- coding: utf-8 -*-
 import os
+from util import util
+
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+os.environ['KIVY_NO_CONSOLELOG'] = '1'
+
+from kivy.config import Config
+Config.set("kivy","log_dir",util.findDataFile("/log"))
+Config.set("kivy","log_level","info")
+Config.set("kivy","log_name","./pielog.txt")
+Config.set('graphics', 'width', '640')
+Config.set('graphics', 'height', '480')
+Config.set('graphics', 'multisamples', '0')
+
 
 from kivy.app import App
 from kivy.core.text import LabelBase
 from kivy.core.text import DEFAULT_FONT
 from kivy.resources import resource_add_path
 from kivy.uix.screenmanager import ScreenManager
-from kivy.config import Config
+from kivy.logger import logging
 
 from view import menu as mu
-from util import util
 from constant.systemconstant import FONT_DIR
 from constant.systemconstant import FONT_FILE_NAME
 
-os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
-os.environ['KIVY_NO_CONSOLELOG'] = '1'
-
-Config.set('graphics', 'width', '640')
-Config.set('graphics', 'height', '480')
-Config.set('graphics', 'multisamples', '0')
 
 resource_add_path(util.findDataFile(FONT_DIR))
 LabelBase.register(DEFAULT_FONT, FONT_FILE_NAME)
 
 class TetoconeScoreApp(App):
     def build(self):
+        logging.debug("App起動")
         self.sm = ScreenManager()
         self.sm.add_widget(
                 mu.MenuScreen(
