895bf3dc740a73cc377a5fd2b3981378ed23325c
Chespins <sync.chespins@gmail.com>
2023-03-01 22:04:25 +0900

    動作確認

--- src/main.py
+++ src/main.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 import os
 from util import util
+from kivy.config import Config
+Config.set('graphics', 'multisamples', '0')
+Config.set("kivy","log_dir",util.findDataFile(""))
 
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 os.environ['KIVY_NO_CONSOLELOG'] = '1'
 
-from kivy.config import Config
-Config.set("kivy","log_dir",util.findDataFile("/log"))
-Config.set("kivy","log_level","info")
-Config.set("kivy","log_name","./pielog.txt")
+
+from kivy.logger import Logger
+from kivy.logger import LOG_LEVELS
+
+Logger.setLevel(LOG_LEVELS["debug"])
 Config.set('graphics', 'width', '640')
 Config.set('graphics', 'height', '480')
-Config.set('graphics', 'multisamples', '0')
-
+Logger.debug("util.findDataFile("")" + util.findDataFile(""))
 
 from kivy.app import App
 from kivy.core.text import LabelBase
 from kivy.core.text import DEFAULT_FONT
 from kivy.resources import resource_add_path
 from kivy.uix.screenmanager import ScreenManager
-from kivy.logger import logging
 
 from view import menu as mu
 from constant.systemconstant import FONT_DIR
@@ -31,13 +33,14 @@ LabelBase.register(DEFAULT_FONT, FONT_FILE_NAME)
 
 class TetoconeScoreApp(App):
     def build(self):
-        logging.debug("App起動")
+        Logger.debug("起動")
         self.sm = ScreenManager()
         self.sm.add_widget(
                 mu.MenuScreen(
                         name='menu'
                     )
             )
+        Logger.debug("起動成功")
         return self.sm
 
 
